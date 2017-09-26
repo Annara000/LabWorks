930f6671e962da128854f5ce09fa5644700ee18f
Annara000 <annamail.000@mail.ru>
2017-09-26 22:15:09 +0300

    Update Main class.kt

--- Lab_2/src/khai/lab2/Main class.kt
+++ Lab_2/src/khai/lab2/Main class.kt
@@ -1,5 +1,13 @@
 package khai.lab2
 
-class Student {
+class Student : Bachelor() {
+    override fun study() {
+        println("Studying $specialization...")
+    }
+
+    override fun drink_something(what: String) {
+        super.drink_something(what)
+        println("Drunk.")
+    }
 
 }
