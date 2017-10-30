6cef5159337ec1a4e7dc093b665aae8555c43c50
Annara000 <annamail.000@mail.ru>
2017-10-30 11:12:31 +0200

    Add files via upload

---
+++ Lab_3/src/khai/lab_3/Main.kt
@@ -0,0 +1,13 @@
+package khai.lab_3
+
+class Implementor {
+
+}
+//LogicMinIndex IterationOddDigits ArrayExceptKL
+fun main(args: Array<String>) {
+    println("Lab 3.")
+
+    val C = Implementor()
+
+    val A = arrayOf(21, 6, -7, 8, 23, 7, 12, 0, -5, 17)
+}
