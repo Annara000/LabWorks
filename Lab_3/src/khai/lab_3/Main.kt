7fe7df19dd0197dab8a11f0dec5a283c540f11f6
Annara000 <annamail.000@mail.ru>
2017-10-30 13:44:14 +0200

    Add files via upload

--- Lab_3/src/khai/lab_3/Main.kt
+++ Lab_3/src/khai/lab_3/Main.kt
@@ -1,13 +1,41 @@
 package khai.lab_3
+//,IterationOddDigits
+class Implementor :LogicMinIndex ,ArrayExceptKL {
 
-class Implementor {
+    override fun min_index(a: Double, b: Double, c: Double): Int {
+        val A1 = arrayOf(a,b,c)
+        println("Порядковый номер меньшего числа")
+        return A1.indexOf(A1.min())
+    }
 
+//    override fun odd_digits(N: Int) {}
+
+    override fun except_kl(A: Array<Double>, K: Int, L: Int) {
+        var AKL=0.0
+        var i=0
+        while (i < A.size) {
+            if (i in K..L) {
+                AKL+=A[i]
+            }
+            i++
+        }
+        val result=A.sum()-AKL
+        println(result)
+    }
 }
-//LogicMinIndex IterationOddDigits ArrayExceptKL
+
 fun main(args: Array<String>) {
     println("Lab 3.")
 
     val C = Implementor()
-
-    val A = arrayOf(21, 6, -7, 8, 23, 7, 12, 0, -5, 17)
+    val A = arrayOf(21.0, 6.0, -7.0, 8.0, 23.0, 7.0, 12.0, 0.0, -5.0, 17.0)
+    println("Testing Logic")
+    println(C.min_index(0.0,0.0, 0.0))
+    println(C.min_index(1.0,-6.0, 5.0))
+    println(C.min_index(-10.47655,0.0009, -20.067))
+//  Testing Iteration
+    println("Testing Array")
+    C.except_kl(A,1,3)
+    C.except_kl(A,0,8)
+    C.except_kl(A,0,9)
 }
